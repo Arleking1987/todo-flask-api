b83436192dc792979f2310e9e403341fd270be58
Yonhatan Ramirez <yonhatan.ramirez@softwareone.com>
2022-09-20 20:55:06 -0500

    Inicio del proyecto

--- ftda.py
+++
@@ -1,5 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials
-
-cred = credentials.Certificate("flask-api-todo-firebase-firebase-adminsdk-whbps-56e8e8ced9.json")
-firebase_admin.initialize_app(cred)
